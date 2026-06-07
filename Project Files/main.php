09defa42c63837ca930ccd038891e59c3f5a04ee
KrishnapriyaBaddula <krishnapriyabaddula@gmail.com>
2026-06-07 11:47:22 +0530

    Delete Project Files directory

--- Project Files/main.php
+++
@@ -1,6 +0,0 @@
-// PHP sends image to Flask API
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://localhost:5000/predict");
-curl_setopt($ch, CURLOPT_POSTFIELDS, ['image' => new CURLFile('test.jpg')]);
-$response = curl_exec($ch);
-curl_close($ch);
--- Project Files/main2.py
+++
@@ -1,3 +0,0 @@
-from tensorflow.keras.applications import ResNet50
-base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3))
-# Add custom layers and compile
